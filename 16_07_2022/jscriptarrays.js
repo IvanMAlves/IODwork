277498269f43f59578de3ec4bf7db7173ed2833c
IvanMAlves <alves.ivan87@gmail.com>
2022-07-16 16:24:44 +1000

    work on 16 jul v2

--- 16_07_2022/jscriptarrays.js
+++ 16_07_2022/jscriptarrays.js
@@ -9,3 +9,18 @@ exampleArray [4] = 55;
 for (let x = exampleArray.length -1 ; x >= 0; x--){
     console.log ('Array position ' + [x] + ' Array Value ' + exampleArray[x]);
 }
+
+let accounts = [[2,8,7],[7,1,3],[1,9,5]];
+
+let highest = 0;
+for (let i of accounts){
+    let total = 0;
+    let accounts = i.length;
+    
+    for (let index = 0; index < accounts; index ++){
+        total += i[index];
+    }
+    highest = total > highest? total : highest;
+}
+
+console.log(highest);
