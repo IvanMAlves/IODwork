b91764d07c0e1f447eaaba8399e2d711aad88e83
IvanMAlves <alves.ivan87@gmail.com>
2022-07-18 19:26:38 +1000

    work on 16 jul v4

--- 16_07_2022/jsonexample.js
+++ 16_07_2022/jsonexample.js
@@ -9,3 +9,46 @@ console.log(book["book_title"]);
 console.log(book["description"]);
 console.log(book["author"]);
 console.log(book["number_of_pages"]);
+
+/*
+const { json } = require("body-parser");
+let jsonObject = `
+{
+    "data": [
+        {
+            "name": "Sammy1",
+            "age": 60
+        },
+        {
+            "name": "Sammy2",
+            "age": 20
+        },
+        {
+            "name": "Sammy3",
+            "age": 80
+        },
+        {
+            "name": "Sammy4",
+            "age": 40
+        }
+    ]
+}`;
+*/
+
+let bodyData = JSON.parse(jsonObject); 
+
+/*
+let response = bodyData.data.sort(function(a, b) {
+    // (a.age < b.age) ? 1 : -1
+    if(a.age < b.age) return 1;
+    else return -1;
+});
+console.log(response);
+*/
+
+let data = bodyData.data;
+data.forEach(item =>{
+    if(!item.name || item.age){
+        throw Error('Missing compultory key: name or age');
+    }
+})
