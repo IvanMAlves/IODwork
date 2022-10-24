197a9c5eac8a9ffa99c670f0b05386e7a3f13671
IvanMAlves <alves.ivan87@gmail.com>
2022-10-24 19:12:47 +1100

    completing lab6 work

--- LabExercise6/exercise6/src/components/calculator.js
+++ LabExercise6/exercise6/src/components/calculator.js
@@ -1,56 +1,76 @@
 import React, { useState } from "react";
 //import TextField from "react";
 
-
 const Calculators = () => {
   const [firstNumber, setFirstNumber] = useState(0);
   const [secondNumber, setSecondNumber] = useState(0);
-  const [Operator, setOperator] = useState('+');
   const [result, setResult] = useState(0);
 
   const handleChangefirst = (event) => {
-    console.log(event);
+    event.preventDefault();
     setFirstNumber(event.target.value);
-    
   };
+
   const handleChangesecond = (event) => {
+    event.preventDefault();
     setSecondNumber(event.target.value);
-    
-  };
-  const handleChangeresult = (event) => {
-    setSecondNumber(event.target.value);
-    
   };
 
   const calculate = (event) => {
-    setOperator(event.target.value);
-    console.log(firstNumber,Operator,secondNumber);
+    event.preventDefault();
+    switch (event.target.operators.value) {
+      case "+":
+        setResult(
+           parseInt(firstNumber) + parseInt(secondNumber),
+        );
+        break;
+      case "-":
+        setResult(
+          parseInt(firstNumber) - parseInt(secondNumber),
+        );
+        break;
+      case "/":
+        setResult(
+           parseInt(firstNumber) / parseInt(secondNumber),
+        );
+        break;
+      case "*":
+        setResult(
+          parseInt(firstNumber) / parseInt(secondNumber),
+        );
+        break;
+      default:
+        break;
+    }
   };
 
   return (
     <div>
-      First Number:
-      <br></br>
-      <input type="text" value={firstNumber} onChange={handleChangefirst} />
-      <br></br>
-      Operator:
-      <br></br>
-      <select name="operators" id="operators">
-        <option selected value="+" >+</option>
-        <option value="-" >-</option>
-        <option value="*" >*</option>
-        <option value="/" >/</option>
-      </select>
-      <br></br>
-      Second Number:
-      <br></br>
-      <input type="text" value={secondNumber} onChange={handleChangesecond} />
-      <br></br>
-      Result:
-      <br></br>
-      <input type="text" value={result} onChange={handleChangeresult} />
-      <br></br>
-      <button onClick={calculate}>calculate</button>
+      <form onSubmit={calculate}>
+        First Number:
+        <br></br>
+        <input type="text" value={firstNumber} onChange={handleChangefirst} />
+        <br></br>
+        Operator:
+        <br></br>
+        <select name="operators" id="operators">
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="*">*</option>
+          <option value="/">/</option>
+        </select>
+        <br></br>
+        Second Number:
+        <br></br>
+        <input type="text" value={secondNumber} onChange={handleChangesecond} />
+        <br></br>
+        Result:
+        <br></br>
+        <input type="text" value={result} />
+        <br></br>
+        <input type="submit" value="submit" />
+        {/* <button onClick={calculate}>calculate</button> */}
+      </form>
     </div>
   );
 };
