765c72853899aa59ab528c5446eb7d48974b8d29
IvanMAlves <alves.ivan87@gmail.com>
2022-11-28 17:09:54 +1100

    udpates to Miniproject 2

--- Miniproject2/miniproject2/src/components/About.js
+++ Miniproject2/miniproject2/src/components/About.js
@@ -4,8 +4,14 @@ const About = () => {
     return (
       <div>
         <h1>About</h1>
-        <p>I hate React it is garbage</p>
-        <p>I have no idea why it is so slow</p>
+        <p>This small app is to demonstrate basic REACT concepts.
+          It connects to a Warhammer Age of Sigmar API which has a list of units under each faction and the size of the base the models belong.
+          The API only has a GET function and no further information can be pushed. They do have a search function, however this search function is not overly strong.
+          This react uses MUI and features such as a navigation bar, sandwich menu (not updated), basic MUI cards and Accordion.
+        </p>
+        <p>
+          This was created by Ivan Alves November 2022.
+        </p>
       </div>
     );
   };
